c847d7316183ea7759b8e741e0bfd0bb63e9c0e4
Tomáš Kratochvíl <kratochviltom00@gmail.com>
2023-08-24 20:58:00 +0200

    Add project files.

---
+++ Streeview player/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Streeview_player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.timerPlay = new System.Windows.Forms.Timer(this.components);
+            this.btnNext = new System.Windows.Forms.Button();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pictureBoxMap = new System.Windows.Forms.PictureBox();
+            this.btnMovie = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.elevationChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elevationChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(13, 519);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(499, 45);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.Value = 1;
+            this.trackBar.ValueChanged += new System.EventHandler(this.trackBar_ValueChanged);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(13, 560);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // timerPlay
+            // 
+            this.timerPlay.Interval = 20;
+            this.timerPlay.Tick += new System.EventHandler(this.timerPlay_Tick);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(94, 560);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Location = new System.Drawing.Point(175, 550);
+            this.trackBarSpeed.Maximum = 50;
+            this.trackBarSpeed.Minimum = 1;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(104, 45);
+            this.trackBarSpeed.TabIndex = 5;
+            this.trackBarSpeed.Value = 1;
+            this.trackBarSpeed.ValueChanged += new System.EventHandler(this.trackBarSpeed_ValueChanged);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(341, 560);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(87, 23);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.Text = "DownloadPics";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(435, 559);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(389, 23);
+            this.progressBar.TabIndex = 7;
+            // 
+            // pictureBoxMap
+            // 
+            this.pictureBoxMap.Location = new System.Drawing.Point(518, 12);
+            this.pictureBoxMap.Name = "pictureBoxMap";
+            this.pictureBoxMap.Size = new System.Drawing.Size(306, 500);
+            this.pictureBoxMap.TabIndex = 8;
+            this.pictureBoxMap.TabStop = false;
+            this.pictureBoxMap.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxMap_Paint);
+            // 
+            // btnMovie
+            // 
+            this.btnMovie.Location = new System.Drawing.Point(737, 530);
+            this.btnMovie.Name = "btnMovie";
+            this.btnMovie.Size = new System.Drawing.Size(87, 23);
+            this.btnMovie.TabIndex = 9;
+            this.btnMovie.Text = "Create movie";
+            this.btnMovie.UseVisualStyleBackColor = true;
+            this.btnMovie.Click += new System.EventHandler(this.btnMovie_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(660, 530);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // elevationChart
+            // 
+            this.elevationChart.BackColor = System.Drawing.Color.Transparent;
+            this.elevationChart.BorderlineColor = System.Drawing.Color.Transparent;
+            this.elevationChart.BorderSkin.BackColor = System.Drawing.Color.Transparent;
+            this.elevationChart.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.elevationChart.ChartAreas.Add(chartArea1);
+            this.elevationChart.Location = new System.Drawing.Point(-10, 426);
+            this.elevationChart.Name = "elevationChart";
+            this.elevationChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series1.BorderColor = System.Drawing.Color.Transparent;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Blue;
+            series1.LabelBackColor = System.Drawing.Color.Transparent;
+            series1.Name = "Elevation";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Name = "Current";
+            this.elevationChart.Series.Add(series1);
+            this.elevationChart.Series.Add(series2);
+            this.elevationChart.Size = new System.Drawing.Size(522, 85);
+            this.elevationChart.TabIndex = 11;
+            this.elevationChart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 595);
+            this.Controls.Add(this.elevationChart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMovie);
+            this.Controls.Add(this.pictureBoxMap);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Streetview player";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elevationChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Timer timerPlay;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.PictureBox pictureBoxMap;
+        private System.Windows.Forms.Button btnMovie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart elevationChart;
+    }
+}
+
---
+++ Streeview player/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+using System.Net;
+using System.Drawing.Imaging;
+using System.Threading;
+using Xabe.FFmpeg;
+using AForge.Video;
+using Accord.Video.FFMPEG;
+using Accord;
+using System.Net.Http;
+using Accord.IO;
+
+namespace Streeview_player
+{
+    public partial class Form1 : Form
+    {
+        private String template = "https://maps.googleapis.com/maps/api/streetview?size=500x500&"+
+                                    "location={0},{1}&fov=120&heading={2}&pitch=0&key=AIzaSyDvbqOmEK6OzG5sbaudEeo8E2H5Puyd4vY";
+        private String templateMap = "https://maps.googleapis.com/maps/api/staticmap?"+
+            "zoom=13&size=306x500&maptype=roadmap&markers=color:red%7Clabel:T%7C{0},{1}&key=AIzaSyDvbqOmEK6OzG5sbaudEeo8E2H5Puyd4vY";
+        private string templateAt = "https://maps.googleapis.com/maps/api/elevation/xml?" +
+            "locations={0}&key=AIzaSyDvbqOmEK6OzG5sbaudEeo8E2H5Puyd4vY";
+        private List<Pos> pos = new List<Pos>();
+        private bool[] downloaded;
+        private Bitmap[] img;
+        private Bitmap[] imgMap;
+        private WebClient client = new WebClient();
+        private const int R = 6378;
+        bool downloading = false;
+        HttpClient clientHttp = new HttpClient();
+        private class Pos
+        {
+            public double lat;
+            public double lon;
+            public double at;
+            public double angle;
+            public double distance;
+            public Pos(double lat, double lon, double at, double dist)
+            {
+                this.lat = lat;
+                this.lon = lon;
+                this.at = at;
+                distance = dist;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            readXML();
+            timerPlay.Interval = 1000 / trackBarSpeed.Value;
+            trackBar.Value = 0;
+        }
+        private void readXML()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            XmlReader reader = XmlReader.Create("C:\\Users\\krato\\Documents\\Google API\\Aktivita-05-07-2021-09-45-14.gpx", settings);
+            double min = 10000;
+            double max = 0;
+            while (reader.Read())
+            {
+                if (reader.Name.Equals("trkpt"))
+                {
+                    XElement el = (XElement)XNode.ReadFrom(reader);
+                    double at = Double.Parse(el.Elements().Where(item => item.Name.LocalName.Equals("ele")).ToArray()[0].Value);
+                    if (at > max) max = at;
+                    if (at < min) min = at;
+                    elevationChart.Series["Elevation"].Points.AddXY(pos.Count, at);
+                    double lat = Double.Parse(el.Attribute("lat").Value);
+                    double lon = Double.Parse(el.Attribute("lon").Value);
+                    double dist = 0;
+                    if (pos.Count > 0)
+                    {
+                        double deltaLon = lon - pos[pos.Count - 1].lon;
+                        double a = Math.Sin(deltaLon) * Math.Cos(lat);
+                        double b = Math.Cos(pos[pos.Count - 1].lat) * Math.Sin(lat)-Math.Sin(pos[pos.Count - 1].lat)*Math.Cos(lat)*Math.Cos(deltaLon);
+                        //pos[pos.Count - 1].angle = Math.Atan(Math.Abs((lat - pos[pos.Count - 1].lat) / (lon - pos[pos.Count - 1].lon))) * 180 / Math.PI;
+                        pos[pos.Count - 1].angle = Math.Atan2(a,b) * 180 / Math.PI;
+
+                        double toRad = Math.PI / 180;
+                        double deltaLat = lat - pos[pos.Count - 1].lat;
+                        a = Math.Pow(Math.Sin(deltaLat * toRad / 2), 2) + Math.Cos(pos[pos.Count - 1].lat * toRad) * Math.Cos(lat* toRad) * Math.Pow(Math.Sin(deltaLon* toRad / 2),2);
+                        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+                        dist = R * c + pos[pos.Count-1].distance;
+                    }
+                    Pos p = new Pos(lat, lon, at, dist);
+                    pos.Add(p);
+                }
+            }
+            elevationChart.ChartAreas[0].AxisY.Minimum = min - 30;
+            elevationChart.ChartAreas[0].AxisY.Maximum = max + 30;
+            trackBar.Maximum = pos.Count - 1;
+            downloaded = new bool[pos.Count];
+            img = new Bitmap[pos.Count];
+            imgMap = new Bitmap[pos.Count];
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (timerPlay.Enabled)
+            {
+                timerPlay.Enabled = false;
+                timerPlay.Stop();
+                btnPlay.Text = "Play";
+            }
+            else
+            {
+                timerPlay.Enabled = true;
+                timerPlay.Start();
+                btnPlay.Text = "Stop";
+            }
+        }
+
+        private void timerPlay_Tick(object sender, EventArgs e)
+        {
+            next();
+        }
+
+        private void trackBar_ValueChanged(object sender, EventArgs e)
+        {
+            if (!downloaded[trackBar.Value] && !downloading)
+            {
+                Stream stream = client.OpenRead(String.Format(template, pos[trackBar.Value].lat, pos[trackBar.Value].lon, pos[trackBar.Value].angle));
+                img[trackBar.Value] = new Bitmap(stream);
+                stream = client.OpenRead(String.Format(templateMap, pos[trackBar.Value].lat, pos[trackBar.Value].lon));
+                imgMap[trackBar.Value] = new Bitmap(stream);
+                downloaded[trackBar.Value] = true;
+                //img[trackBar.Value].Save("Images/" + trackBar.Value + ".jpg", ImageFormat.Jpeg);
+            }
+            pictureBox.Image = img[trackBar.Value];
+            pictureBoxMap.Image = imgMap[trackBar.Value];
+            elevationChart.Series["Current"].Points.Clear();
+            elevationChart.Series["Current"].Points.AddXY(trackBar.Value, pos[trackBar.Value].at);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            next();
+        }
+
+        private void next()
+        {
+            if (trackBar.Value == trackBar.Maximum) return;
+            trackBar.Value++;
+        }
+
+        private void trackBarSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            timerPlay.Interval = 1000 / trackBarSpeed.Value;
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (downloading) return;
+            downloading = true;
+            string path;
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    path = fbd.SelectedPath;
+                }
+                else return;
+            }
+            int index = 0;
+            foreach (var p in pos)
+            {
+                downloadImg(p.lat, p.lon, p.angle, path + "/" + index + ".jpg", index);
+                progressBar.Value = index * 100 / pos.Count;
+                next();
+                index++;
+            }
+            downloading = false;
+        }
+
+        private Bitmap downloadImg(double lat, double lon, double angle, string path, int index)
+        {
+            Stream stream = client.OpenRead(String.Format(template, lat, lon, angle));
+            downloaded[index] = true;
+            Bitmap btm1 = new Bitmap(stream);
+            stream = client.OpenRead(String.Format(templateMap, lat, lon));
+            Bitmap btm2 = new Bitmap(stream);
+            Bitmap tempBitmap = new Bitmap(btm2.Width, btm2.Height);
+            using (Graphics g = Graphics.FromImage(tempBitmap))
+            {
+                // Draw the original bitmap onto the graphics of the new bitmap
+                g.DrawImage(btm2, 0, 0);
+                g.DrawString(pos[index].distance.ToString("#.00") + "km", new Font("Arial", 14), Brushes.Black, 100, 100);
+                g.Flush();
+            }
+            Bitmap concat = createPic(btm1, btm2);
+            concat.Save(path, ImageFormat.Jpeg);
+            downloaded[index] = true;
+            img[index] = btm1;
+            imgMap[index] = btm2;
+            return concat;
+        }
+
+        private Bitmap createPic(Bitmap street, Bitmap map)
+        {
+            int width = street.Width + map.Width;
+            int height = Math.Max(street.Height, map.Height);
+            Bitmap fullBmp = new Bitmap(width, height);
+            Graphics gr = Graphics.FromImage(fullBmp);
+            gr.DrawImage(street, 0, 0, street.Width, street.Height);
+            gr.DrawImage(map, street.Width, 0);
+            return fullBmp;
+        }
+
+        private void createMovie(string path)
+        {
+            int width = pictureBox.Width + pictureBoxMap.Width;
+            int height = pictureBox.Height;
+            var framRate = 10;
+            DirectoryInfo d = new DirectoryInfo(path);
+            using (var vFWriter = new VideoFileWriter())
+            {
+                vFWriter.Open(path + ".avi", width, height, framRate, Accord.Video.FFMPEG.VideoCodec.Default, 10000000);
+                int size = d.GetFiles("*.jpg").Length;
+                for (int i = 0; i < size; i++)
+                {
+                    using (Stream BitmapStream = File.Open(path + "/" + i + ".jpg", FileMode.Open))
+                    {
+                        Image img = Image.FromStream(BitmapStream);
+
+                        Bitmap btm = new Bitmap(img);
+                        vFWriter.WriteVideoFrame(btm);
+                    }
+                }
+                vFWriter.Close();
+            }
+        }
+
+        private void btnMovie_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    createMovie(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void pictureBoxMap_Paint(object sender, PaintEventArgs e)
+        {
+            using (Font myFont = new Font("Arial", 14))
+            {
+                e.Graphics.DrawString(pos[trackBar.Value].distance.ToString(), myFont, Brushes.Green, new System.Drawing.Point(2, 2));
+            }
+        }
+    }
+}
